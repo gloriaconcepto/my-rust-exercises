8e7c18acf7ffb2ea520a56cdc1ec306c9b4d206c
Mmekut Edet <medet@Mmekut-mfonEdets-MacBook-Pro.local>
2022-12-30 12:04:43 +0100

    trait-exercises

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,76 +1,76 @@
-// Silence some warnings so they don't distract from the exercise.
-#![allow(unused_mut, unused_variables)]
+// 1. Define a trait named `Bite`
+//
+// Define a single required method, `fn bite(self: &mut Self)`.  We will call this method when we
+// want to bite something.  Once this trait is defined, you should be able to run the program with
+// `cargo run` without any errors.
+//
+//  trait Bite...
 
-// use std::{sync::WaitTimeoutResult, arch::aarch64::ST};
+
+// 2. Now create a struct named Grapes with a field that tracks how many grapes are left.  If you
+// need a hint, look at how it was done for Carrot at the bottom of this file (you should probably
+// use a different field, though).
+//
+// #[derive(Debug)] // include this line right before your struct definition
+// struct Grapes...
+
+
+// 3. Implement Bite for Grapes.  When you bite a Grapes, subtract 1 from how many grapes are left.
+// If you need a hint, look at how it was done for Carrot at the bottom of this file.
+//
+// impl Bite for...
+
 
 fn main() {
-    // This fancy stuff either gets the first argument as a String, or prints
-    // usage and exits if an argument was not supplied to the program.
-    let mut arg: String = std::env::args().nth(1).unwrap_or_else(|| {
-        println!("Please supply an argument to this program.");
-        std::process::exit(-1);
-    });
+    // Once you finish #1 above, this part should work.
+    let mut carrot = Carrot { percent_left: 100.0 };
+    carrot.bite();
+    println!("I take a bite: {:?}", carrot);
 
-    // 1. Write a function `inspect` that takes a reference to a String, returns nothing, but
-    // prints whether the contents of the String is plural or singular. Then uncomment and run this
-    // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
-    // String reference
+    // 4. Uncomment and adjust the code below to match how you defined your
+    // Grapes struct.
     //
-    inspect(&arg);
+    let mut grapes = Grapes { amount_left: 100 };
+    grapes.bite();
+    println!("Eat a grape: {:?}", grapes);
 
-    // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
-    // the String if it doesn't already end with "s". Then uncomment and run the code below with
-    // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
+    // Challenge: Uncomment the code below. Create a generic `bunny_nibbles`
+    // function that:
+    // - takes a mutable reference to any type that implements Bite
+    // - calls `.bite()` several times
+    // Hint: Define the generic type between the function name and open paren:
+    //       fn function_name<T: Bite>(...)
     //
-    change(&mut arg);
-    //println!("I have many {}", arg);
+    bunny_nibbles(&mut carrot);
+    println!("Bunny nibbles for awhile: {:?}", carrot);
+}
 
-    // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
-    // indicating whether or not the String both starts with a "b" AND contains an "a".
-    // Hint 1: use `.starts_with("b")` and `.contains("a")`
-    // Hint 2: `&&` is the boolean "AND" operator
-    //
-    if eat(arg) {
-       println!("Might be bananas");
-    } else {
-       println!("Not bananas");
+#[derive(Debug)] // This enables using the debugging format string "{:?}"
+struct Carrot {
+    percent_left: f32,
+}
+#[derive(Debug)] // This enables using the debugging format string "{:?}"
+struct Grapes {
+    amount_left:i32,
+}
+impl  Bite for Grapes {
+    fn bite(self: &mut Self) {
+        // Eat 20% of the remaining carrot. It may take awhile to eat it all...
+        self.amount_left -= 1;
     }
-
-    // Try running this program with "boat", "banana", and "grapes" as the arguments :-)
-
-    // Challenge: Write a function "bedazzle" that takes a mutable reference to a String and
-    // ignores what is in the string and replaces the contents of the string with the String
-    // "sparkly". Then uncomment the code below.
-    //
-    // Hint: You will need to dereference the mutable reference in order to assign it a
-    // new value.
-    //
-    let mut material = "mud".to_string();
-    println!("This material is just `{}`.", material);
-    bedazzle(&mut material);
-    println!("Wow! Now the material is `{}`!", material);
 }
-fn inspect(word: &String){
-
-println!("{}",word.ends_with('s'));
-if word.ends_with('s'){
-    println!("this arg {} is a plural word",word);
-}else{
-    println!("this arg {} is not a plural word",word);
-}
-}
-
-fn change(word:&mut String){
-    if ! word.ends_with('s'){
-      word.push_str("s");
+impl Bite for Carrot {
+    fn bite(self: &mut Self) {
+        // Eat 20% of the remaining carrot. It may take awhile to eat it all...
+        self.percent_left *= 0.8;
     }
-    println!("I have many {}",word);
 }
+fn bunny_nibbles<T: Bite>(item: &mut T){
+    item.bite();
 
-fn eat(word:String)->bool{
-  return word.starts_with("b") && word.contains("a");
 }
+trait Bite {
+    fn bite(self: &mut Self);
 
-fn bedazzle(word:&mut String){
-   *word =String::from("sparkly") ;
+   
 }
